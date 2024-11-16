9c360b5a4d782c99edf3109e725a4b091ac6bc6a
BFranco-94 <bryan.franco.velez94@gmail.com>
2024-11-16 02:29:31 -0600

    List Collection

--- app/src/main/java/com/example/collections_classes/ui/routes/Routes.kt
+++ app/src/main/java/com/example/collections_classes/ui/routes/Routes.kt
@@ -1,7 +1,7 @@
 package com.example.collections_classes.ui.routes
 
 object Routes {
-
     const val CLASSES_SCREEN = "classesScreen"
     const val ARRAYS_SCREEN = "arraysScreen"
+    const val LIST_SCREEN = "listScreen"
 }
